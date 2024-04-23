2d84242fa73bfd9a89ba1ef6774429c013bc813a
Dinesh-var <142399483+Dinesh-var@users.noreply.github.com>
2024-04-23 19:51:01 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+// const earnMoneyA = ["Frontend","Backend","Fullstack"];
+// const earnMoneyB = ["AI","Blockchain","DataAnalytics"];
+// const growMoneyA = ["Stocks","Realestate","Fixed income"];
+// const growMoneyB = ["Option","Future"];
+// const givemoney = ["QualityEducation"];
+// console.log(earnMoneyA[0],growMoneyA[1]);
+
+// const earnMoney=[earnMoneyA,earnMoneyB];
+// const growMoney=[growMoneyA,growMoneyB];
+// console.log(earnMoney[0][2],growMoney[1][1]);
+
+// const three=[earnMoney,growMoney,givemoney];
+// console.log(three[1][0][0]);
+// var heading=document.getElementById("Heading")
+// console.log(heading.textContent)
+// heading.textContent="Bye world"
